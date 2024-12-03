16c5d8cf29e7af5c6e10f948121e681253a88490
Hassan Saleh <hassan.saleh@monstar-lab.com>
2024-12-03 21:58:35 +0100

    Revert "fix: Properly dispose of behavious subjects"
    This reverts commit 136ade944035831f6d90f1209ef72a72b4810fa8.

--- src/hooks/useBehaviourSubject.tsx
+++ src/hooks/useBehaviourSubject.tsx
@@ -1,36 +1,29 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useState } from "react";
 import { BehaviorSubject } from "rxjs";
 
 export function useBehaviourSubject<TData>(
   initialValue: TData
 ): [BehaviorSubject<TData>, (newValue: TData) => void] {
-  const ref = useRef<BehaviorSubject<TData>>(new BehaviorSubject(initialValue));
+  const [subject$] = useState(() => new BehaviorSubject(initialValue));
 
   // for cleanup
-  useEffect(() => {
-    if (ref.current != null) {
-      // initial render
-      ref.current.complete();
-    }
+  //   useEffect(() => {
+  //     return () => {
+  //       // close the current subject
+  //       if (!subject$.closed) {
+  //         subject$.complete();
+  //       }
+  //     };
+  //   }, [subject$]);
 
-    ref.current = new BehaviorSubject(ref.current?.value);
-    return () => {
-      // close the current subject
-      if (!ref.current.closed) {
-        ref.current.complete();
+  const setValue = useCallback(
+    (newValue: TData) => {
+      if (!subject$.closed) {
+        subject$.next(newValue);
       }
-    };
-  }, []);
+    },
+    [subject$]
+  );
 
-  const setValue = useCallback((newValue: TData) => {
-    const subject$ = ref.current;
-
-    if (!subject$ || subject$.closed) {
-      return;
-    }
-
-    subject$.next(newValue);
-  }, []);
-
-  return [ref.current, setValue];
+  return [subject$, setValue];
 }
